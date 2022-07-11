b739249eeca13f00404195cd45fa17e7952d2233
Fernando Mesquita <nando.mesquita@me.com>
2022-07-11 20:30:49 -0300

    Created custom reusable objects, a UIButton and TextField.

---
+++ GitHub-App/GitHub-App/GHFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GHFButton.swift
+//  GitHub-App
+//
+//  Created by Fernando Mesquita on 11/07/22.
+//
+
+import UIKit
+
+class GHFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GitHub-App/GitHub-App/GHFTextField.swift
@@ -0,0 +1,39 @@
+//
+//  GHFTextField.swift
+//  GitHub-App
+//
+//  Created by Fernando Mesquita on 11/07/22.
+//
+
+import UIKit
+
+class GHFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "enter a username"
+    }
+}
