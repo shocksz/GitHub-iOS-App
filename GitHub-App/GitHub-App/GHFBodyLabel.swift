58e7ecc375b06a4f5a8c932e0ab459ce76e4e6e2
Fernando Mesquita <nando.mesquita@me.com>
2022-07-12 19:46:12 -0300

    Created two more UI components, a custom title and body label so it can be reused along the project when necessary

---
+++ GitHub-App/GitHub-App/GHFBodyLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GHFBodyLabel.swift
+//  GitHub-App
+//
+//  Created by Fernando Mesquita on 12/07/22.
+//
+
+import UIKit
+
+class GHFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
