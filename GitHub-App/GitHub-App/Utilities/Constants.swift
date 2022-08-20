13cdfee2dbebaf39150991680ccc40cf6f6c86ff
Fernando Mesquita <nando.mesquita@me.com>
2022-08-20 19:13:04 -0300

    Created the Info Item View

--- GitHub-App/GitHub-App/Utilities/Constants.swift
+++ GitHub-App/GitHub-App/Utilities/Constants.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum SFSymbols {
     static let location = "mapping.and.ellipse"
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
 }
