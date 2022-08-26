3748407648270ed519895f5672611459afb60aa5
Fernando Mesquita <nando.mesquita@me.com>
2022-08-26 09:51:41 -0300

    Created a button to add favorites

---
+++ GitHub-App/GitHub-App/Managers/PersistenceManager.swift
@@ -0,0 +1,76 @@
+//
+//  PersistenceManager.swift
+//  GitHub-App
+//
+//  Created by Fernando Mesquita on 25/08/22.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add
+    case remove
+}
+
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GHFErrorMessage?) -> Void) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    
+                    retrievedFavorites.append(favorite)
+                    
+                case .remove:
+                    retrievedFavorites.removeAll { $0.login == favorite.login }
+                }
+                
+                completed(save(favorites: favorites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+            
+        }
+    }
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GHFErrorMessage>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GHFErrorMessage? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+    
+}
