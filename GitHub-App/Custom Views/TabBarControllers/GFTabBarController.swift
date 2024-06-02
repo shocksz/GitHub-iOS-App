b32bed1b7dfe3b315eb8255636a06b8192397d7d
Fernando Mesquita <nando.mesquita@me.com>
2024-06-02 11:44:50 -0300

    Scene Delegate optimization

---
+++ GitHub-App/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  GitHub-App
+//
+//  Created by Fernando Mesquita on 02/06/24.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNavigationController(), createFavoritesNavigationController()]
+    }
+   
+    
+    func createSearchNavigationController() -> UINavigationController {
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNavigationController() -> UINavigationController {
+        let favoritesListVC = FavoritesListViewController()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+
+}
